48ebbcfecb360baaedd804e39121b23b7dc3dbde
Vladtsrez <Competitionvladinf@gmail.com>
2024-02-15 20:55:50 +0200

    add

---
+++ js/task-1.js
@@ -0,0 +1,19 @@
+function isEnoughCapacity(products, containerSize) {
+  const value = Object.values(products);
+  let totalCount = 0;
+
+  for (const values of value) {
+    totalCount += values;
+  }
+
+  if (containerSize >= totalCount) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14));
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7));
